02730f205510856828770d0891166ace1b94a4c9
Arlann <handikaarlan@gmail.com>
2024-10-01 10:51:02 +0700

    feat: add ip address to console

--- public/script.js
+++ public/script.js
@@ -1,61 +1,61 @@
-const socket = io();
-const boardElement = document.querySelectorAll(".box");
-const resetButton = document.getElementById("play-again");
-const resultsElement = document.getElementById("results");
-let turn = "X";
-let isGameOver = false;
-let playerSymbol = null;
-let isServer = false;
+const socket = io()
+const boardElement = document.querySelectorAll(".box")
+const resetButton = document.getElementById("play-again")
+const resultsElement = document.getElementById("results")
+let turn = "X"
+let isGameOver = false
+let playerSymbol = null
+let isServer = false
 
 boardElement.forEach((box) => {
   box.addEventListener("click", () => {
-    const index = box.getAttribute("data-index");
+    const index = box.getAttribute("data-index")
     if (!isGameOver && box.innerHTML === "" && playerSymbol === turn) {
-      socket.emit("move", { index });
+      socket.emit("move", { index })
     }
-  });
-});
+  })
+})
 
 socket.on("init", (data) => {
   data.board.forEach((val, index) => {
-    boardElement[index].innerHTML = val || "";
-  });
-  turn = data.currentPlayer;
-  playerSymbol = data.playerSymbol;
-  isServer = data.isServer;
+    boardElement[index].innerHTML = val || ""
+  })
+  turn = data.currentPlayer
+  playerSymbol = data.playerSymbol
+  isServer = data.isServer
 
   // Adjust turn indicator
-  document.querySelector(".bg").style.left = turn === "X" ? "0" : "85px";
-  resetButton.style.display = isServer ? "inline" : "none";
-});
+  document.querySelector(".bg").style.left = turn === "X" ? "0" : "85px"
+  resetButton.style.display = isServer ? "inline" : "none"
+})
 
 socket.on("update", (data) => {
   data.board.forEach((val, index) => {
-    boardElement[index].innerHTML = val || "";
-  });
-  turn = data.currentPlayer;
-  document.querySelector(".bg").style.left = turn === "X" ? "0" : "85px";
-});
+    boardElement[index].innerHTML = val || ""
+  })
+  turn = data.currentPlayer
+  document.querySelector(".bg").style.left = turn === "X" ? "0" : "85px"
+})
 
 socket.on("gameOver", (data) => {
-  isGameOver = true;
-  resultsElement.innerHTML = data.winner ? `${data.winner} wins!` : "Draw!";
+  isGameOver = true
+  resultsElement.innerHTML = data.winner ? `${data.winner} wins!` : "Draw!"
   if (isServer) {
-    resetButton.style.display = "inline";
+    resetButton.style.display = "inline"
   }
-});
+})
 
 resetButton.addEventListener("click", () => {
-  socket.emit("resetGame");
-});
+  socket.emit("resetGame")
+})
 
 socket.on("reset", () => {
-  isGameOver = false;
-  resultsElement.innerHTML = "";
+  isGameOver = false
+  resultsElement.innerHTML = ""
   boardElement.forEach((box) => {
-    box.innerHTML = "";
-    box.style.removeProperty("background-color");
-    box.style.color = "#fff";
-  });
-  resetButton.style.display = "none";
-});
+    box.innerHTML = ""
+    box.style.removeProperty("background-color")
+    box.style.color = "#fff"
+  })
+  resetButton.style.display = "none"
+})
--- server.js
+++ server.js
@@ -1,84 +1,99 @@
-import express from 'express';
-import { Server } from 'socket.io';
-import { createServer } from 'http';
+import express from 'express'
+import os from 'os'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
-let board = Array(9).fill(null);
-let currentPlayer = 'X';
-let players = {};
-let playerAddresses = {};
+function getWiFiIPAddress() {
+    const interfaces = os.networkInterfaces()
+    if (interfaces['Wi-Fi']) {
+        for (let iface of interfaces['Wi-Fi']) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address
+            }
+        }
+    }
+    return "127.0.0.1"
+}
 
-app.use(express.static('public'));
+const serverIP = getWiFiIPAddress()
+
+let board = Array(9).fill(null)
+let currentPlayer = 'X'
+let players = {}
+let playerAddresses = {}
+
+app.use(express.static('public'))
 
 function checkWinner(board) {
     const winningCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
-    ];
+    ]
 
     for (let combo of winningCombinations) {
-        const [a, b, c] = combo;
+        const [a, b, c] = combo
         if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-            return board[a];
+            return board[a]
         }
     }
-    return null;
+    return null
 }
 
 function isBoardFull(board) {
-    return board.every(cell => cell !== null);
+    return board.every(cell => cell !== null)
 }
 
 io.on('connection', (socket) => {
-    const playerIP = socket.handshake.address;
+    const playerIP = socket.handshake.address
 
     if (playerAddresses[playerIP]) {
-        players[socket.id] = playerAddresses[playerIP];
+        players[socket.id] = playerAddresses[playerIP]
     } else if (Object.keys(players).length < 2) {
-        const symbol = Object.keys(players).length === 0 ? 'X' : 'O';
-        players[socket.id] = symbol;
-        playerAddresses[playerIP] = symbol;
+        const symbol = Object.keys(players).length === 0 ? 'X' : 'O'
+        players[socket.id] = symbol
+        playerAddresses[playerIP] = symbol
     } else {
-        socket.emit('spectator');
+        socket.emit('spectator')
     }
 
-    socket.emit('init', { board, currentPlayer, playerSymbol: players[socket.id], isServer: players[socket.id] === 'X' });
+    socket.emit('init', { board, currentPlayer, playerSymbol: players[socket.id], isServer: players[socket.id] === 'X' })
 
     socket.on('move', (data) => {
-        if (players[socket.id] !== currentPlayer || board[data.index]) return;
+        if (players[socket.id] !== currentPlayer || board[data.index]) return
 
-        board[data.index] = currentPlayer;
-        const winner = checkWinner(board);
+        board[data.index] = currentPlayer
+        const winner = checkWinner(board)
 
         if (winner) {
-            io.emit('gameOver', { winner });
-            board = Array(9).fill(null);
-            currentPlayer = 'X';
+            io.emit('gameOver', { winner })
+            board = Array(9).fill(null)
+            currentPlayer = 'X'
         } else if (isBoardFull(board)) {
-            io.emit('gameOver', { winner: null });
-            board = Array(9).fill(null);
-            currentPlayer = 'X';
+            io.emit('gameOver', { winner: null })
+            board = Array(9).fill(null)
+            currentPlayer = 'X'
         } else {
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
         }
-        io.emit('update', { board, currentPlayer });
-    });
+        io.emit('update', { board, currentPlayer })
+    })
 
     socket.on('resetGame', () => {
-        board = Array(9).fill(null);
-        currentPlayer = 'X';
-        io.emit('reset');
-    });
+        board = Array(9).fill(null)
+        currentPlayer = 'X'
+        io.emit('reset')
+    })
 
     socket.on('disconnect', () => {
-        delete players[socket.id];
-    });
-});
+        delete players[socket.id]
+    })
+})
 
 server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000")
-});
+    console.log(`Server running on http://${serverIP}:3000`)
+})

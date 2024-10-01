25c1b2a5ed12fc46f6c43a7d9c70c52cc35355d4
Arlann <handikaarlan@gmail.com>
2024-10-01 10:35:10 +0700

    chore: update console message

--- server.js
+++ server.js
@@ -80,5 +80,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000')
+    console.log("Server running on http://localhost:3000")
 });
